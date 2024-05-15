3103048624ce5e18aa721c97d4e75b42e94fd113
David Wenk <davidwenk@icloud.com>
2024-05-15 12:22:22 +0200

    edit and delete article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,12 +18,19 @@ class ArticlesController < ApplicationController
   end
 
   def edit
+    @article = Article.find(params[:id]) 
   end
 
   def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+    redirect_to articles_path(@article)
   end
 
   def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path, status: :see_other
   end
 
   private
